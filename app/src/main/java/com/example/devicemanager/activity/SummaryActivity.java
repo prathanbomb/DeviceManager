c0db80a675afc2d295acb4dddca3467ea501e170
athkrit <athkrit_tae@hotmail.com>
2019-06-19 17:15:09 +0700

    Fix UI summary and add back button

--- app/src/main/java/com/example/devicemanager/activity/SummaryActivity.java
+++ app/src/main/java/com/example/devicemanager/activity/SummaryActivity.java
@@ -1,8 +1,10 @@
 package com.example.devicemanager.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.devicemanager.R;
 import com.example.devicemanager.fragment.SummaryFragment;
@@ -23,6 +25,17 @@ public class SummaryActivity extends AppCompatActivity {
     }
 
     private void initInstance() {
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
