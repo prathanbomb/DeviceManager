10a419f04b4f09e7018ac83b3e40a7ea41eb083e
athkrit <athkrit_tae@hotmail.com>
2019-06-20 13:18:34 +0700

    Search delete

--- app/src/main/java/com/example/devicemanager/activity/MainActivity.java
+++ app/src/main/java/com/example/devicemanager/activity/MainActivity.java
@@ -15,9 +15,6 @@ import com.example.devicemanager.R;
 import com.example.devicemanager.fragment.MainFragment;
 import com.example.devicemanager.fragment.SearchFragment;
 
-import android.app.SearchManager;
-import android.widget.SearchView;
-import android.widget.SearchView.OnQueryTextListener;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -36,54 +33,5 @@ public class MainActivity extends AppCompatActivity {
     private void initInstances() {
 
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu, menu);
-//        MenuItem searchItem = menu.findItem(R.id.action_search);
-//
-//        return true;
-        getMenuInflater().inflate(R.menu.menu,menu);
-        MenuItem menuItem = menu.findItem(R.id.action_search);
-        androidx.appcompat.widget.SearchView searchView = (androidx.appcompat.widget.SearchView) menuItem.getActionView();
-        searchView.setOnCloseListener(new androidx.appcompat.widget.SearchView.OnCloseListener() {
-            @Override
-            public boolean onClose() {
-                Toast.makeText(MainActivity.this, "asasaxacav", Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
-        searchView.setOnQueryTextListener(new androidx.appcompat.widget.SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
-                getSupportFragmentManager().beginTransaction()
-                        .attach(SearchFragment.newInstance())
-                        .detach(MainFragment.newInstance())
-                        .commit();
-                return true;
-            }
 
-            @Override
-            public boolean onQueryTextChange(String s) {
-                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
-                getSupportFragmentManager().beginTransaction()
-                        .attach(SearchFragment.newInstance())
-                        .detach(MainFragment.newInstance())
-                        .commit();
-                return true;
-            }
-
-        });
-
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            getSupportFragmentManager().beginTransaction().remove(SearchFragment.newInstance()).commit();
-            //finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
