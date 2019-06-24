e8426079f77eb20a780fb17ce8a5a8c0c95e2aa9
Arada <arachpsc115@gmail.com>
2019-06-24 09:02:07 +0700

    Update app design #2 using cardView to show list

--- app/src/main/java/com/example/devicemanager/fragment/MainFragment.java
+++ app/src/main/java/com/example/devicemanager/fragment/MainFragment.java
@@ -1,20 +1,12 @@
 package com.example.devicemanager.fragment;
 
-import android.app.SearchManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
-import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import com.example.devicemanager.R;
