eb7105ea0036e9de943d752b8a51077415eb8537
Arada <arachpsc115@gmail.com>
2019-06-25 17:45:59 +0700

    Add RegisterActivity update LoginActivity

--- app/src/main/java/com/example/devicemanager/fragment/LoginFragment.java
+++ app/src/main/java/com/example/devicemanager/fragment/LoginFragment.java
@@ -2,11 +2,13 @@ package com.example.devicemanager.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,12 +23,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.w3c.dom.Text;
+
 public class LoginFragment extends Fragment {
 
     private EditText etEmail, etPassword;
     private FirebaseAuth mAuth;
     private String strEmail, strPassword;
     private Button btnSubmit;
+    private TextView tvRegister;
 
     public static LoginFragment newInstance(){
         LoginFragment fragment = new LoginFragment();
@@ -61,37 +66,61 @@ public class LoginFragment extends Fragment {
         btnSubmit = view.findViewById(R.id.btnLoginSubmit);
 
         btnSubmit.setOnClickListener(onClickBtnSubmit);
+
+        tvRegister = view.findViewById(R.id.tvRegister);
+        tvRegister.setOnClickListener(onClickRegister);
     }
 
     private void userLogin() {
         strEmail = etEmail.getText().toString().trim();
         strPassword = etPassword.getText().toString().trim();
 
-        mAuth = FirebaseAuth.getInstance();
-        mAuth.signInWithEmailAndPassword(strEmail, strPassword)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
-                    Toast.makeText(Contextor.getInstance().getContext(),
-                            "Login Success", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getActivity(), MainActivity.class);
-                    startActivity(intent);
-                    getActivity().finish();
-                }
-                else {
-                    Toast.makeText(Contextor.getInstance().getContext(),
-                            "Failed to Login", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        if (checkForm(strEmail, strPassword)){
+            mAuth = FirebaseAuth.getInstance();
+            mAuth.signInWithEmailAndPassword(strEmail, strPassword)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(Contextor.getInstance().getContext(),
+                                        "Login Success", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getActivity(), MainActivity.class);
+                                startActivity(intent);
+                                getActivity().finish();
+                            }
+                            else {
+                                Toast.makeText(Contextor.getInstance().getContext(),
+                                        "Failed to Login", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
 
+    }
+
+    private boolean checkForm(String strEmail, String strPassword) {
+        if(TextUtils.isEmpty(strEmail) || TextUtils.isEmpty(strPassword)){
+            Toast.makeText(getActivity(), "Please insert all the fields", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 
     private View.OnClickListener onClickBtnSubmit = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             userLogin();
+        }
+    };
+
+    private View.OnClickListener onClickRegister = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contentContainer, RegisterFragment.newInstance())
+                    .commit();
         }
     };
 
---
+++ app/src/main/java/com/example/devicemanager/fragment/RegisterFragment.java
@@ -0,0 +1,164 @@
+package com.example.devicemanager.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.devicemanager.R;
+import com.example.devicemanager.activity.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class RegisterFragment extends Fragment {
+
+    private EditText etEmail, etPassword, etCode;
+    private String strEmail, strPassword, strCode;
+    private boolean state;
+    private FirebaseAuth mAuth;
+    private Button btnSubmit;
+    private TextView tvLogin;
+
+    public RegisterFragment() {
+    }
+
+    public static RegisterFragment newInstance() {
+        RegisterFragment fragment = new RegisterFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        initInstances(view);
+        return view;
+    }
+
+    private void initInstances(View view) {
+        etEmail = view.findViewById(R.id.etRegEmail);
+        etPassword = view.findViewById(R.id.etRegPassword);
+        etCode = view.findViewById(R.id.etRegInviteCode);
+        btnSubmit = view.findViewById(R.id.btnRegSubmit);
+        tvLogin = view.findViewById(R.id.tvRegLogin);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSubmit.setOnClickListener(onClickSubmit);
+        tvLogin.setOnClickListener(onClickLogin);
+    }
+
+    private void registerUser() {
+        strEmail = etEmail.getText().toString().trim();
+        strPassword = etPassword.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(strEmail, strPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(getActivity(), "Register Success", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                    startActivity(intent);
+                    getActivity().finish();
+                }
+            }
+        });
+    }
+
+    private boolean checkCode(){
+        strCode = etCode.getText().toString().trim();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance()
+                .getReference().child("InvitationCode");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot s : dataSnapshot.getChildren()){
+                    String code = s.child("Code").getValue(String.class);
+                    if (code != null && strCode.matches(code)){
+                        state = true;
+                        break;
+                    }
+                    else {
+                        state = false;
+                        Log.d("Firebase", "Data not matched");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("Firebase", databaseError.getMessage());
+                state = false;
+            }
+        });
+        return state;
+    }
+
+    private boolean checkForm(){
+        strEmail = etEmail.getText().toString().trim();
+        strPassword = etPassword.getText().toString().trim();
+        strCode = etCode.getText().toString().trim();
+        String regEmail = strEmail.substring(strEmail.indexOf("@") + 1);
+
+        if (TextUtils.isEmpty(strEmail) || TextUtils.isEmpty(strPassword) || TextUtils.isEmpty(strCode)){
+            Toast.makeText(getActivity(), "Please insert all the fields", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if (!regEmail.matches(getResources().getString(R.string.digio_email))){
+            Toast.makeText(getActivity(), "Wrong E-Mail address", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if (strPassword.length() < 6) {
+            Toast.makeText(getActivity(), "Password must contain at least 6 letters", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    private View.OnClickListener onClickSubmit = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (checkForm() && checkCode()){
+                registerUser();
+            }
+        }
+    };
+
+    private View.OnClickListener onClickLogin = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.contentContainer, LoginFragment.newInstance())
+                    .commit();
+        }
+    };
+
+}
