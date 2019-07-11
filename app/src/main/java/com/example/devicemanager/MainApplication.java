c88b72dd9e19bc54202c49f867b1339a327c41c5
athkrit <athkrit_tae@hotmail.com>
2019-07-11 14:41:29 +0700

    crashlytic

--- app/src/main/java/com/example/devicemanager/MainApplication.java
+++ app/src/main/java/com/example/devicemanager/MainApplication.java
@@ -3,12 +3,15 @@ package com.example.devicemanager;
 import android.app.Application;
 
 import com.example.devicemanager.manager.Contextor;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 
 public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         Contextor.getInstance().init(getApplicationContext());
     }
 
